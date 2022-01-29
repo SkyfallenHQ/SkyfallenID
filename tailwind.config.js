faf24cb4c8c7b725560254c9fde5a2c03fa4bf02
Yigit Kerem Oktay <yigitkerem@theskyfallen.com>
2022-01-30 00:04:05 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,16 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      transitionProperty: {
+        'width': 'width',
+        'spacing': 'padding,margin'
+      },
+    },
   },
   variants: {
     extend: {},
+    width: ["responsive", "hover", "focus"],
   },
   plugins: [],
 }
